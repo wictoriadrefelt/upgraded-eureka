3e536496347a7e4c55ba99b167e06f0d4bf5fa81
Fredrik <fredrik_olssons@live.se>
2023-01-10 13:11:34 +0100

    somesing wierd written

--- client/src/components/pages/Product.jsx
+++ client/src/components/pages/Product.jsx
@@ -1,14 +1,9 @@
 import { getProducts } from "../../actions/productAction";
 import { Link } from "react-router-dom";
+import ProductCard from "./ProductCard";
 
 const Products = ({ product }) => {
-  getProducts();
-  return (
-    <Link className="productCard" to={product._id}>
-      <p>{product.name}</p>
-      <p>{product.price}</p>
-    </Link>
-  );
+  return <ProductCard></ProductCard>;
 };
 
 export default Products;
