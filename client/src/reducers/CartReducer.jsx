bc495bd6bb94e8e8ed3d18354142d21c24c8b0de
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2023-01-16 12:52:11 +0100

    added cartReducer

---
+++ client/src/reducers/CartReducer.jsx
@@ -0,0 +1,24 @@
+import { ADD_TO_CART } from "../constants/cartActionTypes";
+
+export const productReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const item = action.payload;
+      const ifItemExists = state.cartItems.find(
+        (i) => i.product === item.product
+      );
+      if (ifItemExists) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((i) =>
+            i.product === ifItemExists.product ? item : i
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+  }
+};
