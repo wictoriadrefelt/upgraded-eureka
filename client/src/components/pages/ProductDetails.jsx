3b77a907cc8c46261695545cb781c8e5626cc5ea
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2023-01-16 11:27:02 +0100

    quantity for buttons

--- client/src/components/pages/ProductDetails.jsx
+++ client/src/components/pages/ProductDetails.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getSingleProduct } from "../../actions/productAction";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    if (product.unit <= quantity) {
+      return;
+    }
+
+    const qty = quantity + 1;
+    setQuantity(qty);
+    console.log(qty);
+  };
+
+  const decreseQuantity = () => {
+    if (1 >= quantity) {
+      return;
+    }
+    const qty = quantity - 1;
+    setQuantity(qty);
+    console.log(qty);
+  };
+
   const { product } = useSelector((state) => state.singleProduct);
   useEffect(() => {
     dispatch(getSingleProduct(id));
   }, [dispatch, id]);
-  console.log(product);
+
   return (
     <div>
       {product.name}
-      <button>-</button>
-      <input value="1" type="number" />
-      <button>+</button>
+      <button onClick={decreseQuantity}>-</button>
+      <input value={quantity} type="number" />
+      <button onClick={increaseQuantity}>+</button>
       <button>Add to Cart</button>
       Avaliable:
       <p className={product.unit < 1 ? "black" : "white"}>
