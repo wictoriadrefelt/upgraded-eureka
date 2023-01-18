b18905de0fb2dfefe5994ee1da3b2dc12ccfbe10
Fredrik <fredrik_olssons@live.se>
2023-01-18 19:06:58 +0100

    styling changes to mostly cart, and a few other meaningless things

--- client/src/components/pages/CartItemCard.jsx
+++ client/src/components/pages/CartItemCard.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../Styles/cartItemCard.css";
 
 const CartItemCard = ({ item, deleteItem }) => {
   return (
-    <div>
-      <img alt="productImage" />
-      <Link to={`/product/${item.product}`}>{item.name}</Link>
-      <span>{`Price: ${item.price}`}</span>
-      <p onClick={() => deleteItem(item.product)}>Remove</p>
-    </div>
+    <>
+      <div className="cartItemCard">
+        <Link to={`/product/${item.product}`}>
+          <img className="cartImg" src={item.image} alt="productImage" />
+        </Link>
+        <Link to={`/product/${item.product}`}>
+          <p>{item.name}</p>
+        </Link>
+      </div>
+      <div className="removeBtn">
+        <p onClick={() => deleteItem(item.product)}>Remove</p>
+      </div>
+    </>
   );
 };
 
