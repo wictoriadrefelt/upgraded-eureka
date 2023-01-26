9f5bd0203afb3c5e24bba594d9a26ca5ec519ec3
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2023-01-26 23:23:15 +0100

    user adds to created order

--- api/routes/orderRoute.js
+++ api/routes/orderRoute.js
@@ -7,13 +7,17 @@ const {
   updateOrderStatus,
 } = require("../controllers/orderController");
 
+const { authorizedRoles, isAuthenticated } = require("../middleware/auth");
+
 const router = express.Router();
 
-router.route("/order/new").post(newOrder);
+router.route("/order/new").post(isAuthenticated, newOrder);
 router.route("/order/:id").get(getOrderById);
 router.route("/orders").get(getAllOrders);
-router.route("/admin/orders").get(getAllOrders);
-router.route("/admin/orders/:id").delete(deleteOrder);
-router.route("/admin/orders/:id").put(updateOrderStatus);
+router.route("/admin/orders").get(authorizedRoles("admin"), getAllOrders);
+router.route("/admin/orders/:id").delete(authorizedRoles("admin"), deleteOrder);
+router
+  .route("/admin/orders/:id")
+  .put(authorizedRoles("admin"), updateOrderStatus);
 
 module.exports = router;
