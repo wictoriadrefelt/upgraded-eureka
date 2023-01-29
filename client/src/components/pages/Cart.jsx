471a3e27751387e274f359a20bc81cba5ec9a015
Fredrik <fredrik_olssons@live.se>
2023-01-29 17:27:33 +0100

    cartchanges

--- client/src/components/pages/Cart.jsx
+++ client/src/components/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { Link } from "react";
+import React, { Link, useState } from "react";
 import "../../Styles/cart.css";
 import CartItemCard from "./CartItemCard";
 import { useSelector, useDispatch } from "react-redux";
@@ -6,11 +6,12 @@ import { addItemsToCart, removeItemFromCart } from "../../actions/cartAction";
 import Header from "../view/Header";
 import tallpipe from "./../../assets/Bg/tallpipe.png";
 import shortpipe from "./../../assets/Bg/shortpipe.png";
-import flower from "./../../assets/Bg/flower.png";
-import Terms from "./Terms";
+import flower from "./../../assets/Bg/flower1.jpg";
+import TermsAndConditions from "./Terms";
 import "../../Styles/terms.css";
 
 const Cart = () => {
+  document.title = "IneedIT Cartpage";
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
 
@@ -25,7 +26,7 @@ const Cart = () => {
 
   const decreaseQuantity = (id, quantity, unit) => {
     const newQty = quantity - 1;
-    if (unit <= quantity || 1 > quantity) {
+    if (1 >= quantity) {
       return;
     } else {
       dispatch(addItemsToCart(id, newQty));
@@ -36,102 +37,112 @@ const Cart = () => {
     dispatch(removeItemFromCart(id));
   };
 
+  const [accepted, setAccepted] = useState(false);
+
+  const handleAccept = (accept) => {
+    setAccepted(accept);
+  };
+
   const goToCheckout = () => {
     window.location.replace("/shipping");
     // history.push("/login?redirect=shipping")
   };
-  {
-    return (
-      <>
-        <Header />
-        {cartItems.length === 0 ? (
-          <>
-            <div className="emptyCart">"Nothing to see here"</div>
-            <Link to="/products">View our amazing collection</Link>
-          </>
-        ) : (
-          /// PLACE DIVS FOR EMPTY CART HERE AFTER LINE 35
-          <>
-            <div className="cartPageMain">
-              <div className="toCheckout">
-                <p className="checkoutBtn">Insert Coins To Begin</p>
-                <br />
-                <p className="checkoutClickMe" onClick={goToCheckout}>
-                  ...Click here...
-                </p>
-              </div>
-              <Terms></Terms>
-              <div className="leftFooterImg">
-                <img className="imgLeft" src={tallpipe} alt="tallpipe" />
-                <img className="flower" src={flower} alt="" />
-              </div>
-              <div className="rightFooterImg">
-                <img className="imgRight" src={shortpipe} alt="shortpipe" />
-                <img className="flower" src={flower} alt="" />
-              </div>
-              <div className="cartContainer">
-                <div className="cartContent">
-                  <h2 className="cartTitle">Kundvagn</h2>
-                  {cartItems &&
-                    cartItems.map((item) => (
-                      <div className="cartItem" key={item.product}>
-                        <CartItemCard item={item} deleteItem={deleteItem} />
-                        <div className="cartDetails">
-                          <div className="btns">
-                            <button
-                              className="amount add"
-                              onClick={() =>
-                                decreaseQuantity(
-                                  item.product,
-                                  item.quantity,
-                                  item.unit
-                                )
-                              }
-                            >
-                              -
-                            </button>
-                            <p className="amountNumber">{item.quantity}</p>
-                            <button
-                              className="amount decrease"
-                              onClick={() =>
-                                increaseQuantity(
-                                  item.product,
-                                  item.quantity,
-                                  item.unit
-                                )
-                              }
-                            >
-                              +
-                            </button>
-                          </div>
-                          <div className="itemPrice">
-                            <p>{item.price * item.quantity}</p>
-                          </div>
-                        </div>
-                        {/* PRICE FOR ONE PRODUCT  */}
-                      </div>
-                    ))}
 
-                  {/*PRICE FOR TOTAL CART*/}
-                  <div className="totalPriceCart">
-                    <p>
-                      {cartItems.reduce(
-                        (acc, item) => acc + item.quantity * item.price,
-                        0
-                      )}
-                    </p>
-                  </div>
-                </div>
+  return (
+    <>
+      <Header />
+      <>
+        <div className="cartPageMain">
+          <div className="toCheckout">
+            <TermsAndConditions onAccept={handleAccept}>
+              <div className="visible">
+                {/* <button disabled={!accepted}>Proceed to Checkout</button> */}
               </div>
-              <div className="stars"></div>
-              <div className="twinkleMask"></div>
-              <div className="twinkleMask2"></div>
+            </TermsAndConditions>
+            <p className="checkoutBtn">Insert Coins To Begin</p>
+            <br />
+            <p
+              className="checkoutClickMe"
+              disabled={!accepted}
+              onClick={goToCheckout}
+            >
+              ...Click here...
+            </p>
+          </div>
+
+          <div className="leftFooterImg">
+            <img className="imgLeft" src={tallpipe} alt="tallpipe" />
+            <img className="flower-8s" src={flower} alt="" />
+          </div>
+          <div className="rightFooterImg">
+            <img className="imgRight" src={shortpipe} alt="shortpipe" />
+            <img className="flower-5s" src={flower} alt="" />
+          </div>
+          <div className="cartContainer">
+            <div className="cartContent">
+              <div className="fixed-center">
+                <h2 className="cartTitle">Kundvagn</h2>
+              </div>
+              {cartItems &&
+                cartItems.map((item) => (
+                  <div className="cartItem" key={item.product}>
+                    <CartItemCard item={item} deleteItem={deleteItem} />
+                    <div className="cartDetails">
+                      <div className="btns">
+                        <button
+                          className="amount add"
+                          onClick={() =>
+                            decreaseQuantity(
+                              item.product,
+                              item.quantity,
+                              item.unit
+                            )
+                          }
+                        >
+                          -
+                        </button>
+                        <p className="amountNumber">{item.quantity}</p>
+                        <button
+                          className="amount decrease"
+                          disabled={item.quantity >= item.unit}
+                          onClick={() =>
+                            increaseQuantity(
+                              item.product,
+                              item.quantity,
+                              item.unit
+                            )
+                          }
+                        >
+                          +
+                        </button>
+                      </div>
+                      <div className="itemPrice">
+                        <p>{item.price * item.quantity}</p>
+                      </div>
+                    </div>
+                    {/* PRICE FOR ONE PRODUCT  */}
+                  </div>
+                ))}
+              {/*PRICE FOR TOTAL CART*/}
+              <div className="totalPriceCart">
+                <p className="priceFontSize">Total price:</p>
+                <p className="priceFontSize">
+                  {cartItems.reduce(
+                    (acc, item) => acc + item.quantity * item.price,
+                    0
+                  )}
+                </p>
+                <p className="priceFontSize"> :-</p>
+              </div>
             </div>
-          </>
-        )}
+          </div>
+          <div className="stars"></div>
+          <div className="twinkleMask"></div>
+          <div className="twinkleMask2"></div>
+        </div>
       </>
-    );
-  }
+    </>
+  );
 };
 
 export default Cart;
